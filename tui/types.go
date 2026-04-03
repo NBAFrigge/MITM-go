c10a83ebb9308a9637eed7d3440c3af25e88f22f
Frigge <frigerioalessandro659@gmail.com>
2026-04-03 16:51:02 +0200

    old code cleaning

--- tui/types.go
+++ tui/types.go
@@ -1,8 +1,6 @@
 package tui
 
 import (
-	"fmt"
-
 	"httpDebugger/pkg/sessiondata"
 )
 
@@ -21,31 +19,4 @@ type ProxyStatusMsg struct {
 
 type SessionsUpdatedMsg struct {
 	Sessions []*sessiondata.Session
-}
-
-type SessionItem struct {
-	Session *sessiondata.Session 
-}
-
-func (i SessionItem) FilterValue() string {
-	return i.Session.Request.URL + " " + i.Session.Request.Method
-}
-
-func (i SessionItem) Title() string {
-	status := ""
-	if i.Session.Response != nil && i.Session.Response.StatusCode > 0 {
-		status = fmt.Sprintf(" [%d]", i.Session.Response.StatusCode)
-	}
-	return fmt.Sprintf("%s %s%s", i.Session.Request.Method, i.Session.Request.URL, status)
-}
-
-func (i SessionItem) Description() string {
-	sessionType := "HTTP"
-	if i.Session.Type == sessiondata.WebSocketSession {
-		sessionType = "WebSocket"
-	}
-	return fmt.Sprintf("Type: %s | Duration: %s | %s",
-		sessionType,
-		i.Session.Duration.String(),
-		i.Session.Timestamp.Format("15:04:05"))
 }
